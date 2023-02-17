ec795a2292f998b56dd8248e3b905df591af7891
igorv <igorvinson@gmail.com>
2023-02-17 12:23:33 -0500

    fix cors problem

--- server.js
+++ server.js
@@ -9,8 +9,8 @@ const client = require("twilio")(accountSid, authToken);
 const app = express();
 
 app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
     res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
     res.setHeader('Access-Control-Allow-Credentials', true);
     next();
