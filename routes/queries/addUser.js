f9c87e4d63bd858900516332ee811f709fde29d5
igorv <igorvinson@gmail.com>
2023-03-11 13:30:46 -0500

    add check in addUser query

--- routes/queries/addUser.js
+++ routes/queries/addUser.js
@@ -8,10 +8,13 @@ const addUser = async (req, res) => {
         if (existingUser.rows.length > 0) {
             return res.send('User with this email already exists');
         }
-
+        // check if username or email is null or empty don't insert
+        if (!username || !email) {
+            return res.send('Username or email is empty');
+        }
         // insert new user
         const result = await pool.query('INSERT INTO users (username, email) VALUES ($1, $2)', [username, email]);
-        res.status(200).send('USER CREATED');
+        res.status(200).send('User added');
     }
     catch (error) {
         res.status(400).send(error);
--- routes/queries/delAllContacts.js
+++ routes/queries/delAllContacts.js
@@ -2,7 +2,8 @@ const {pool} = require("../../db");
 
 const delAllContacts =async (req, res) => {
     try {
-        await pool.query('TRUNCATE users RESTART IDENTITY');
+        await pool.query('TRUNCATE users CASCADE;')
+        await pool.query('ALTER SEQUENCE users_id_seq RESTART WITH 1;');
         res.status(200).send('USERS DELETED');
     }
     catch (error) {
