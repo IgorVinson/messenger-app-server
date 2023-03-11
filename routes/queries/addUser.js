d95e84bbe8d42c659e98d8dfa8e50d42ef0b7e1e
igorv <igorvinson@gmail.com>
2023-03-11 14:21:15 -0500

    add check in addUser query

--- routes/queries/addUser.js
+++ routes/queries/addUser.js
@@ -3,18 +3,24 @@ const {pool} = require("../../db");
 const addUser = async (req, res) => {
     const { username, email } = req.body;
     try {
+        // check if username or email is null or empty don't insert
+        if (!username || !email) {
+            return res.status(200).send('Username or email is empty');
+        }
+
         // check if user already exists
         const existingUser = await pool.query('SELECT * FROM users WHERE email = $1', [email]);
         if (existingUser.rows.length > 0) {
-            return res.send('User with this email already exists');
+            //find user by email and return it
+            const user = await pool.query('SELECT * FROM users WHERE email = $1', [email]);
+            return res.status(200).send(user.rows[0]);
         }
-        // check if username or email is null or empty don't insert
-        if (!username || !email) {
-            return res.send('Username or email is empty');
-        }
+
         // insert new user
-        const result = await pool.query('INSERT INTO users (username, email) VALUES ($1, $2)', [username, email]);
-        res.status(200).send('User added');
+        await pool.query('INSERT INTO users (username, email) VALUES ($1, $2)', [username, email]);
+        const user = await pool.query('SELECT * FROM users WHERE email = $1', [email]);
+        return res.status(200).send(user.rows[0]);
+
     }
     catch (error) {
         res.status(400).send(error);
